6224292591a373fce77cb267f77d853c7607c6c3
OLEG <oleg@powerc.ru>
2020-01-09 13:28:30 +0300

    Init

--- asteriskami.go
+++ asteriskami.go
@@ -2,75 +2,73 @@ package main
 
 import (
 	"bufio"
+	"encoding/xml"
+	"flag"
+	"fmt"
 	"log"
 	"net"
-	"fmt"
-	"strings"
-	"encoding/xml"
 	"strconv"
-	"flag"
+	"strings"
 )
 
 type result struct {
-	Channel string		`xml:"channel"`
-	Value string		`xml:"value"`
-	Valuelookup string	`xml:"ValueLookup"`
+	Channel     string `xml:"channel"`
+	Value       string `xml:"value"`
+	Valuelookup string `xml:"ValueLookup"`
 }
 
 type prtgbody struct {
-	XMLName xml.Name	`xml:"prtg"`
-	Res []result		`xml:"result"`
+	XMLName xml.Name `xml:"prtg"`
+	Res     []result `xml:"result"`
 }
 
-
-type amiparams struct{
-	AsteriskIP string
+type amiparams struct {
+	AsteriskIP   string
 	AsteriskPort string
-	AMIUser string
-	AMIPassword string
+	AMIUser      string
+	AMIPassword  string
 }
 
 type siptrunkregstatus struct {
-	SipUsername string
+	SipUsername  string
 	SipRegStatus string
 }
 
-func RespToMap(stresp string)map[string]string{
+func RespToMap(stresp string) map[string]string {
 	var rmap map[string]string
-	rmap = make(map[string]string,0)
-	ass := strings.Split(stresp,"\r\n")
-	for _,assentry := range ass{
-		assformap := strings.Split(assentry,":")
-		if len(assformap) == 2{
+	rmap = make(map[string]string, 0)
+	ass := strings.Split(stresp, "\r\n")
+	for _, assentry := range ass {
+		assformap := strings.Split(assentry, ":")
+		if len(assformap) == 2 {
 
 			rmap[assformap[0]] = strings.TrimSpace(assformap[1])
 		}
 	}
 	return rmap
-	}
+}
 
 func main() {
-	username := flag.String("u","prtg","asterisk AMI username")
-	passwd := flag.String("p","prtg","aterisk AMI password")
-	aaip := flag.String("i","","aterisk IP address")
-	aaport := flag.String("dp","5038","asterisk MII port, default 5038")
+	username := flag.String("u", "prtg", "asterisk AMI username")
+	passwd := flag.String("p", "prtg", "aterisk AMI password")
+	aaip := flag.String("i", "", "aterisk IP address")
+	aaport := flag.String("dp", "5038", "asterisk MII port, default 5038")
 	flag.Parse()
 
 	var ststat []siptrunkregstatus
-	sm := amiparams{*aaip,*aaport,*username,*passwd}
-	conn,err  := net.Dial("tcp",sm.AsteriskIP+":"+sm.AsteriskPort)
+	sm := amiparams{*aaip, *aaport, *username, *passwd}
+	conn, err := net.Dial("tcp", sm.AsteriskIP+":"+sm.AsteriskPort)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 	scanner := bufio.NewScanner(conn)
 
-	AuthStr := "Action: login\r\nUsername: "+sm.AMIUser+"\r\n"+"Secret: "+sm.AMIPassword+"\r\nEvents: off\r\nActionID: 23456063340\r\n\r\n"
+	AuthStr := "Action: login\r\nUsername: " + sm.AMIUser + "\r\n" + "Secret: " + sm.AMIPassword + "\r\nEvents: off\r\nActionID: 23456063340\r\n\r\n"
 
 	SipRegistryStr := "Action: SIPshowregistry\r\nActionID: 23456063340\r\n\r\n"
 
 	LogooffStr := "Action: logoff\r\n\r\n"
-
 
 	for scanner.Scan() {
 		conn.Write([]byte(AuthStr))
@@ -85,7 +83,7 @@ func main() {
 		}
 
 		if i := strings.Index(string(data), "\r\n\r\n"); i >= 0 {
-			return i + len("\r\n\r\n"), data[0:i+len("\r\n\r\n")], nil
+			return i + len("\r\n\r\n"), data[0 : i+len("\r\n\r\n")], nil
 		}
 
 		if atEOF {
@@ -102,16 +100,16 @@ func main() {
 
 	authrespm := RespToMap(AuthRespT)
 
-	if authrespm["Message"] == "Authentication accepted"{
+	if authrespm["Message"] == "Authentication accepted" {
 		conn.Write([]byte(SipRegistryStr))
-		for scanner2.Scan(){
+		for scanner2.Scan() {
 			respt := scanner2.Text()
 			sipregm := RespToMap(respt)
 
-			if val,ok := sipregm["ActionID"];ok{
-				if val == "23456063340"{
-					if val,ok := sipregm["EventList"];ok{
-						if val == "start"{
+			if val, ok := sipregm["ActionID"]; ok {
+				if val == "23456063340" {
+					if val, ok := sipregm["EventList"]; ok {
+						if val == "start" {
 							EventListFlag = true
 							continue
 						}
@@ -119,29 +117,28 @@ func main() {
 				}
 			}
 
-			if val,ok := sipregm["ActionID"];ok{
-				if val == "23456063340"{
-					if val,ok := sipregm["EventList"];ok{
-						if val == "Complete"{
+			if val, ok := sipregm["ActionID"]; ok {
+				if val == "23456063340" {
+					if val, ok := sipregm["EventList"]; ok {
+						if val == "Complete" {
 							break
 						}
-					}else {
+					} else {
 
-
-						if val,ok := sipregm["ActionID"];ok{
-							if val == "23456063340"{
-								if val,ok := sipregm["EventList"];ok{
-									if val == "Complete"{
+						if val, ok := sipregm["ActionID"]; ok {
+							if val == "23456063340" {
+								if val, ok := sipregm["EventList"]; ok {
+									if val == "Complete" {
 										break
 									}
-								}else {
+								} else {
 
-									if EventListFlag{
-										if val,ok := sipregm["Event"];ok{
-											if val == "RegistryEntry"{
+									if EventListFlag {
+										if val, ok := sipregm["Event"]; ok {
+											if val == "RegistryEntry" {
 												st := sipregm["State"]
 												un := sipregm["Username"]
-												ststat = append(ststat,siptrunkregstatus{un,st})
+												ststat = append(ststat, siptrunkregstatus{un, st})
 											}
 										}
 									}
@@ -157,17 +154,16 @@ func main() {
 
 	conn.Write([]byte(LogooffStr))
 	scanner2.Scan()
-
 	conn.Close()
 
 	var rd1 []result
 
-	for _,rres := range ststat{
+	for _, rres := range ststat {
 		itstat := 0
-		if rres.SipRegStatus == "Registered"{
+		if rres.SipRegStatus == "Registered" {
 			itstat = 1
 		}
-		rd1 = append(rd1,result{rres.SipUsername,strconv.Itoa(itstat),"Asterisk"})
+		rd1 = append(rd1, result{rres.SipUsername, strconv.Itoa(itstat), "Asterisk"})
 	}
 
 	mt1 := &prtgbody{Res: rd1}
--- readme.txt
+++ readme.txt
@@ -1,4 +1,15 @@
 -u AMI username
 -p aterisk AMI password
 -i aterisk IP address
--dp asterisk MII port, default 5038
+-dp asterisk AMI port, default 5038
+
+=======================================================================================================================
+Sensor for PRTG monitoring system.
+For monitoring SIP trunk with registration.
+Asterisk version lower than 12 do not have REST API but have AMI interface, this sensor utilize AMI.
+
+License GPL
+Author Volkov Oleg, PowerC 2019
+oleg@powerc.ru
+=======================================================================================================================
+
